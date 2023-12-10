cc24bc01796e60441c9bcf6445cc365e8b1bfc51
benjazzy <22528361+benjazzy@users.noreply.github.com>
2023-12-10 16:44:28 -0500

    Day 8 part 1

--- 2023/day-08/src/part1.rs
+++ 2023/day-08/src/part1.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
+use itertools::{FoldWhile, Itertools};
 use nom::bytes::complete::{tag, take_until1};
 use nom::character::complete::{alphanumeric1, newline};
 use nom::combinator::value;
 use nom::multi::separated_list1;
-use nom::sequence::{pair, separated_pair};
+use nom::sequence::{delimited, pair, separated_pair};
 use nom::IResult;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -19,24 +20,29 @@ struct Node<'a> {
     right: &'a str,
 }
 
-pub fn parse_node_pair(input: &str) -> IResult<&str, (&str, &str)> {}
+impl<'a> Node<'a> {
+    pub fn turn(&self, turn: &Turn) -> &str {
+        match turn {
+            Turn::Left => &self.left,
+            Turn::Right => &self.right,
+        }
+    }
+}
 
-fn parse_line(input: &str) -> IResult<&str, (&str, (&str, &str))> {
-    separated_pair(
-        alphanumeric1,
-        tag(" = "),
-        value(
-            separated_pair(alphanumeric1, tag(", "), alphanumeric1),
-            (tag("("), take_until1(")"), tag("(")),
-        ),
+pub fn parse_node_pair(input: &str) -> IResult<&str, (&str, &str)> {
+    delimited(
+        tag("("),
+        separated_pair(alphanumeric1, tag(", "), alphanumeric1),
+        tag(")"),
     )(input)
 }
 
-fn parse_map(input: &str) -> IResult<&str, HashMap<&str, Node>> {
-    println!("Input: {input}");
-    let (rest, lines) = separated_list1(newline, parse_line)(input)?;
+fn parse_line(input: &str) -> IResult<&str, (&str, (&str, &str))> {
+    separated_pair(alphanumeric1, tag(" = "), parse_node_pair)(input)
+}
 
-    println!("Rest: {rest}");
+fn parse_map(input: &str) -> IResult<&str, HashMap<&str, Node>> {
+    let (rest, lines) = separated_list1(newline, parse_line)(input)?;
 
     let nodes: HashMap<&str, Node> = lines
         .into_iter()
@@ -62,12 +68,45 @@ fn parse(input: &str) -> IResult<&str, (Vec<Turn>, HashMap<&str, Node>)> {
     Ok((rest, (turns, nodes)))
 }
 
+fn process_loop(turns: Vec<Turn>, nodes: HashMap<&str, Node>) -> u64 {
+    let mut next = "AAA";
+    let mut count = 1_u64;
+    loop {
+        let result = turns
+            .iter()
+            .fold_while((count, next), |(count, prev), turn| {
+                let next = nodes
+                    .get(prev)
+                    .expect("Node was not found in map")
+                    .turn(&turn);
+
+                // println!("Turn: {:?}, {prev} maps to {next}", turn);
+
+                if next == "ZZZ" {
+                    FoldWhile::Done((count, prev))
+                } else {
+                    FoldWhile::Continue((count + 1, next))
+                }
+            });
+
+        match result {
+            FoldWhile::Continue((c, n)) => {
+                next = n;
+                count = c;
+            }
+            FoldWhile::Done((c, _)) => return c,
+        }
+    }
+}
+
 pub fn process(input: &str) -> String {
     let (_, (turns, nodes)) = parse(input).expect("Input should be valid");
 
     println!("\nTurns: {:?}\n\nNodes: {:?}", turns, nodes);
 
-    "".to_string()
+    let count = process_loop(turns, nodes);
+
+    count.to_string()
 }
 
 #[cfg(test)]
